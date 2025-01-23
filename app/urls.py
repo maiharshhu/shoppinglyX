08c47946c910164384311a7f189702ec95e60078
Maiharshhu <hj.harsh440@gmail.com>
2025-01-23 21:42:10 +0530

    work on registration page and login page

--- app/urls.py
+++ app/urls.py
@@ -5,7 +5,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     # path('', views.home),
     path('', views.ProductView.as_view(), name='home'),
-    path('product-detail/', views.product_detail, name='product-detail'),
+    path('product-detail/<int:pk>',
+         views.ProductDetailView.as_view(), name='product-detail'),
     path('cart/', views.add_to_cart, name='add-to-cart'),
     path('buy/', views.buy_now, name='buy-now'),
     path('profile/', views.profile, name='profile'),
@@ -13,8 +14,11 @@ urlpatterns = [
     path('orders/', views.orders, name='orders'),
     path('changepassword/', views.change_password, name='changepassword'),
     path('mobile/', views.mobile, name='mobile'),
+    path('mobile/<slug:data>', views.mobile, name='mobiledata'),
     path('login/', views.login, name='login'),
-    path('registration/', views.customerregistration,
-         name='customerregistration'),
+    path('registration/', views.CustomerRegistrationView.as_view(),
+         name="customerregistration"),
+    #     path('registration/', views.customerregistration,
+    #          name='customerregistration'),
     path('checkout/', views.checkout, name='checkout'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- app/views.py
+++ app/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from django.views import View
 from .models import Customer, Product, Cart, OrderPlace
-# Create your views here.
+from .forms import CustomerRegistrationForm
+from django.contrib import messages
 
+# Create your views here.
 
 # def home(request):
 #     return render(request, 'app/home.html')
+
 
 class ProductView(View):
     def get(self, request):
@@ -22,8 +25,13 @@ class ProductView(View):
         return render(request, 'app/home.html', {'mobiles': mobiles, 'laptops': laptops, 'topwears': topwears, 'bottomwears': bottomwears, 'accessories': accessories, 'electronics': electronics, 'homedecors': homedecors, 'footwears': footwears, 'bags': bags, 'beautyproducts': beautyproducts})
 
 
-def product_detail(request):
-    return render(request, 'app/productdetail.html')
+# def product_detail(request):
+#     return render(request, 'app/productdetail.html')
+
+class ProductDetailView(View):
+    def get(self, request, pk):
+        product = Product.objects.get(pk=pk)
+        return render(request, 'app/productdetail.html', {'product': product})
 
 
 def add_to_cart(request):
@@ -50,21 +58,46 @@ def change_password(request):
     return render(request, 'app/changepassword.html')
 
 
+# def mobile(request, data=None):
+#     if data == None:
+#         mobiles = Product.objects.filter(category='M')
+#     elif data == 'Redmi' or data == "Samsung":
+#         mobiles = Product.objects.filter(category='M').filter(brand=data)
+#     return render(request, 'app/mobile.html', {'mobiles': mobiles})
+
 def mobile(request, data=None):
-    # if data == None:
-    #     mobiles = Product.objects.filter(category='M')
-    # elif data == 'Redmi' or data == "Samsung":
-    #     mobiles = Product.objects.filter(category='M').filter
-    #     (brand=data)
-    return render(request, 'app/mobile.html')  # , {'mobiles': mobiles})
+    if data == None:
+        mobiles = Product.objects.filter(category='M')
+    elif data in ['Redmi', 'Samsung', 'Apple', 'Vivo', 'Oppo', 'Realme', 'Motorola', 'Google']:
+        mobiles = Product.objects.filter(category='M', brand=data)
+    elif data == 'below':
+        mobiles = Product.objects.filter(
+            category='M').filter(discounted_price__lt=10000)
+    elif data == 'above':
+        mobiles = Product.objects.filter(
+            category='M').filter(discounted_price__gt=10000)
+    return render(request, 'app/mobile.html', {'mobiles': mobiles})
 
 
 def login(request):
     return render(request, 'app/login.html')
 
 
-def customerregistration(request):
-    return render(request, 'app/customerregistration.html')
+# def customerregistration(request):
+#     return render(request, 'app/customerregistration.html')
+
+class CustomerRegistrationView(View):
+    def get(self, request):
+        form = CustomerRegistrationForm()
+        return render(request, 'app/customerregistration.html', {'form': form})
+
+    def post(self, request):
+        form = CustomerRegistrationForm(request.POST)
+        if form.is_valid():
+            messages.success(
+                request, 'Congratulations!! Registered Successfully')
+            form.save()
+        return render(request, 'app/customerregistration.html', {'form': form})
 
 
 def checkout(request):
